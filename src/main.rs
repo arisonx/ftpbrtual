294e83fecc18039ce8d003f53b9643316ddac9e1
Arison Reis <arisontechp@gmail.com>
2023-11-20 11:50:55 -0300

    feat: adding FTP connection

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::{
     io::{self, BufRead},
 };
 
+use ftp::FtpStream;
+
 struct Config {
     ip: String,
     port: String,
     file: String,
     num_threads: String,
+    username: String,
 }
 
 impl Default for Config {
@@ -18,6 +21,7 @@ impl Default for Config {
             port: String::new(),
             file: String::new(),
             num_threads: String::new(),
+            username: String::new(),
         }
     }
 }
@@ -42,21 +46,41 @@ fn main() {
         // wordlist
         if arg == "-w" && &index + 1 < args.len() {
             config.file = args[index + 1].clone();
-            process_wordlist(&config.file);
+        }
+
+        // username
+        if arg == "-u" && &index + 1 < args.len() {
+            config.username = args[index + 1].clone();
+        }
+
+        if arg == "-t" && &index + 1 < args.len() {
+            config.num_threads = args[index + 1].clone();
         }
     }
+
+    process_wordlist(config);
 }
 
-fn process_wordlist(wordlist_path: &str) {
+fn process_wordlist(config : Config) {
     //open the wordlist file
-    if let Ok(file) = File::open(wordlist_path) {
+    if let Ok(file) = File::open(config.file) {
         //read file rows
         for line in io::BufReader::new(file).lines() {
             if let Ok(word) = line {
-                println!("word: {}\n", word);
+                println!("[...] TRYING: {}", word);
+                ftp_connect(&config.ip, &config.port, &config.username, &word);
             }
         }
     } else {
         print!("ERROR: Error opening wordlist file.")
     }
+}
+
+fn ftp_connect(ip: &str, port: &str, username: &str, password: &str) {
+    let mut ftp_stream =
+        FtpStream::connect(ip).unwrap_or_else(|err| panic!("Error connect {}", err));
+    let _ = ftp_stream
+        .login(username, password)
+        .unwrap_or_else(|err| panic!("Error login {}", err));
+    let _ = ftp_stream.quit();
 }
